f7a47412918ced596a96d7e812ee54c684fce3d5
Mohan Kumar N <125173156+MohanReddy05@users.noreply.github.com>
2025-12-07 13:54:19 +0530

    database exploration
    Added SQL queries to explore the database structure and details of tables.

--- analysis/01_data_exploration.sql
+++ analysis/01_data_exploration.sql
@@ -1,1 +1,55 @@
 
+/*
+====================================================
+        Explore the Databaase Structure
+====================================================
+*/
+
+use gold;
+show tables;
+
+/*
+ * There are three tables in database namely 
+ * dim_customer
+ * dim_product
+ * fact_sales
+*/
+
+-- Query to display the details of fact_sales table
+
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    ORDINAL_POSITION,
+    DATA_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'gold'
+  AND TABLE_NAME = 'fact_sales';
+
+-- Query to display the details of dim_customer table
+
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    ORDINAL_POSITION,
+    DATA_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'gold'
+  AND TABLE_NAME = 'dim_customer';
+
+-- Query to display the details of dim_product table
+
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    ORDINAL_POSITION,
+    DATA_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'gold'
+  AND TABLE_NAME = 'dim_product';
